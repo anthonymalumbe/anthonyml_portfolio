4ccf0c8a9911b43aacbe239adbc789e3c9fcf45c
AnthonyML <112323852+anthonymalumbe@users.noreply.github.com>
2024-01-20 18:05:16 +0000

    Create Launch_Deep_Learning_VM_using_gcloud.sh
    Launch a notebook instance and get URL to Jupyter running on it

---
+++ 04_notebooks/Script_to_create_a_GCP_notebook_instance/Launch_Deep_Learning_VM_using_gcloud.sh
@@ -0,0 +1,26 @@
+IMAGE=--image-family=tf-latest-cpu
+INSTANCE_NAME=dlvm
+GCP_LOGIN_NAME=google-cloud-customer@gmail.com  # CHANGE THIS
+STARTUP_SCRIPT="git clone https://github.com/GoogleCloudPlatform/data-science-on-gcp"
+
+echo "Launching $INSTANCE_NAME"
+gcloud compute instances create ${INSTANCE_NAME} \
+      --machine-type=n1-standard-2 \
+      --scopes=https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/userinfo.email \
+      ${IMAGE} \
+      --image-project=deeplearning-platform-release \
+      --boot-disk-device-name=${INSTANCE_NAME} \
+      --metadata="proxy-user-mail=${GCP_LOGIN_NAME}"
+echo "Looking for Jupyter URL on $INSTANCE_NAME"
+while true; do
+   proxy=$(gcloud compute instances describe ${INSTANCE_NAME} 2> /dev/null | grep dot-datalab-vm)
+   if [ -z "$proxy" ]
+   then
+      echo -n "."
+      sleep 1
+   else
+      echo "done!"
+      echo "$proxy"
+      break
+   fi
+done
